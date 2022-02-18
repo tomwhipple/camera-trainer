773cd8dc3717553c431142ed21bf5e61e6512c79
Tom Whipple <mail@tomwhipple.com>
2022-02-18 12:51:59 -0600

    Initial Commit

---
+++ CameraTrainer/CameraTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraTrainerApp.swift
+//  CameraTrainer
+//
+//  Created by Tom Whipple on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct CameraTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
